07bef0f8b52e967b20ae2bb801702b41ce1d1086
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2020-01-04 23:01:02 +0100

    solve bug with rotation vector

--- Core/RotationVectorToEulerAxis.m
+++ Core/RotationVectorToEulerAxis.m
@@ -1,5 +1,9 @@
 function [axis,angle] = RotationVectorToEulerAxis(rotvec)
 %EulerAxisToQuaternion Transform a Rotation Vector to Euler Axis
 angle=norm(rotvec);
-axis=rotvec/angle;
+if(angle==0)
+    axis=[1;0;0];
+else
+    axis=rotvec/angle;
+end
 end
